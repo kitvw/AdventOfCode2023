04833bd5712b7e763779e772e01715fb7dcabbdf
Kit Vander Wilt <KitVanderWilt@gmail.com>
2023-12-11 18:39:17 -0700

    Problem 8 input files

--- src/main/java/problems/Problem_08.java
+++ src/main/java/problems/Problem_08.java
@@ -1,10 +1,11 @@
 package problems;
 
+import common.PuzzleInput;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class Problem_08 {
     public static void main(String[] args) {
-
+        String input = PuzzleInput.fileContents("sample_08.txt");
     }
 }
